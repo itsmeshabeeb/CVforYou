f1fcc32bf21a38121442a73b5c3cb33fd7acc37c
King8111 <mshabeeb4@gmail.com>
2021-12-01 13:59:02 +0530

    update

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("index",views.index, name="index"),
+    path("",views.index, name="index"),
     path('master',views.index1,name='master'),
     path("templates",views.templateFun,name="templates"),
     path("personal",views.personal,name="Personal"),
